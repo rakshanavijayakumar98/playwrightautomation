02cc6a9b6887752efa2bbd373969e401506ca94c
rakshanavijayakumar98 <rakshanaabinesh@gmail.com>
2026-05-27 12:48:18 +0530

    Refactor CreateLead.spec.ts for clarity

--- PlaywrightHomeAssg/CreateLead.spec.ts
+++ PlaywrightHomeAssg/CreateLead.spec.ts
@@ -7,7 +7,7 @@ test("Create Lead",async({page})=>{
     
     await page.locator(`text='CRM/SFA'`).click() //text locator type is used here, we can use text=CRM/SFA also
     await page.locator(`text='Leads'`).click() 
-    await page.waitForTimeout(2000) //wait for 2 seconds to load the page completely, we can use waitForSelector also to wait for a specific element to be visible
+    await page.waitForTimeout(2000) //wait for 2 seconds to load the page completely
     await page.locator(`text='Create Lead'`).first().click()
 
     await page.locator("#createLeadForm_companyName").fill("TestLeaf") //id locator type is used here, we can use [id='createLeadForm_companyName'] also
@@ -19,8 +19,7 @@ test("Create Lead",async({page})=>{
     await page.locator("#createLeadForm_departmentName").fill("QA")
     await page.locator("#createLeadForm_primaryPhoneNumber").fill("9999999999")
 
-    await page.waitForTimeout(2000) //wait for 2 seconds to load the page completely, we can use waitForSelector also to wait for a specific element to be visible
-
+    await page.waitForTimeout(2000) 
     await page.locator(".smallSubmit").click() //class locator type is used here, we can use [class='smallSubmit'] also
 
 })
